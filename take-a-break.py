40fa281cb73fd3f1783ff3256d3246d843ed0374
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-07-02 22:56:54 +0600

    Add files via upload

---
+++ take-a-break.py
@@ -0,0 +1,26 @@
+import webbrowser
+import time
+
+def OpenWebsite():
+	# Open A Website in new tab
+	webbrowser.open('http://google.com', new=2)
+	print "Done Opening URL."
+
+def StopBreak():
+	print 'Done Break. Start Working Again.'
+
+def main():
+	for i in xrange(3):
+		# Start Break
+		# Open URL after one min
+	    time.sleep(1*60) # In Second
+	    OpenWebsite()
+
+	    # End Break
+	    time.sleep(1*60)
+	    StopBreak()
+
+if __name__ == '__main__':
+	main()
+
+

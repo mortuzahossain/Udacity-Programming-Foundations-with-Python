26adbf6b96085082bc34abe428150394f85c9f7f
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-07-06 21:38:40 +0600

    Add files via upload

--- Profanity Editor/main.py
+++ Profanity Editor/main.py
@@ -10,6 +10,13 @@ def checkProdanity(text_to_check):
 	connection = urllib.urlopen('http://www.wdylike.appspot.com/?q='+text_to_check)
 	output = connection.read()
 	connection.close()
+	# For Better output
+	if 'true' in output:
+		output = 'Prodanity Aleart.'
+	elif 'false' in output:
+		output = 'This Document is clean.'
+	else:
+		output = 'Can not scan the doc please try again.'
 	return output
 
 if __name__ == '__main__':
